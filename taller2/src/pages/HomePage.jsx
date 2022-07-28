a0c37e313fc3b467b0d9bf6579d32a6c7436ec4a
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-07-27 19:35:20 -0500

    Recarga de página al termianr juego.

--- taller2/src/pages/HomePage.jsx
+++ taller2/src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import Start from "../components/Start";
+import useCards from "../hooks/useCards";
 
 const HomePage = () => {
+  const {endGame, setEndGame} = useCards();
+
+  if(endGame) {
+    window.location.reload();
+    setEndGame(false);
+  }
+
   return <Start />;
 };
 
